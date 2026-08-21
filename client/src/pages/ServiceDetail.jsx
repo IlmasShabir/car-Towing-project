405053ae593176580a3631d59cbd0cfe220dadd4
Ilmas shabir <almasshabir967@gmail.com>
2026-08-21 22:15:19 +0500

    Show service name in detail heading

--- client/src/pages/ServiceDetail.jsx
+++ client/src/pages/ServiceDetail.jsx
@@ -76,7 +76,7 @@ const ServiceDetail = () => {
           </Reveal>
 
           <Reveal direction="right" delay={150} className="service-detail-content">
-            <h2 className="service-detail-title">{service.seoTitle || service.name}</h2>
+            <h2 className="service-detail-title">{ service.name}</h2>
             <p className="service-detail-desc">{service.longDesc}</p>
             <ul className="service-detail-features">
               {normalizeFeatures(service.features).map((f) => (
